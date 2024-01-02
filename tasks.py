d9a2a8eb7413886a9289793fcd4832d6a70d8b0d
Kari Harju <kari@robocorp.com>
2024-01-02 19:20:30 +0200

    Simple update to robocorp logging, readme update and License fixed

--- tasks.py
+++ tasks.py
@@ -1,16 +1,21 @@
+from robocorp.tasks import task
 from pdf2image import convert_from_path
 
 
 def convert_pdf_to_images(pdf_path):
+    """
+    Handles single or multipaged PDFs to PNG images using 'pdf2image' -library
+    The result images are placed in the output -folder
+    """
     images = convert_from_path(pdf_path)
     for index, image in enumerate(images):
         image.save(f'output/{pdf_path}-{index}.png')
 
 
+@task
 def task():
+    """
+    Converting PDF files to image is usefull in OCR cases.
+    """
     convert_pdf_to_images('example-invoice.pdf')
     convert_pdf_to_images('example-multipage.pdf')
-
-
-if __name__ == "__main__":
-    task()
